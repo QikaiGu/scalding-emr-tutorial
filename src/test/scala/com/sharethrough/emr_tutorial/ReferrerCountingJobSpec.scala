fa69f3be2a1f7867bae05278470feb1a8800852a
Rob Slifka <robert.slifka@gmail.com>
2013-09-29 02:32:25 -0700

    Removing training wheels

--- src/test/scala/com/sharethrough/emr_tutorial/ReferrerCountingJobSpec.scala
+++ src/test/scala/com/sharethrough/emr_tutorial/ReferrerCountingJobSpec.scala
@@ -4,18 +4,27 @@ import org.specs2.mutable.Specification
 import com.twitter.scalding._
 
 class ReferrerCountingJobSpec extends Specification {
-  // Scalding implicit conversions (Scala => Cascading)
   import Dsl._
 
   val lines = List(
   )
 
+  /*
+    TODO: Wrapping a JobTest in the Specs2 "should" syntax does not work here,
+    contrary to the working example provided in the Scalding Wiki.  I'm assuming
+    one or both of Specs2 or a Scalding version bump is to be looked into.
+
+    What are the semantics of 'should' with respect to parallel execution?
+  */
   JobTest("com.sharethrough.emr_tutorial.ReferrerCountingJob").
     arg("input", "inputFile").
     arg("output", "outputFile").
     source(JsonLine("inputFile", List("body")), lines).
+
+    // TODO This tuple has to match the output format
     sink[(String, Int)](Tsv("outputFile")) { outputBuffer =>
 
+      // TODO: This example block is inside our job now! (not outside like it used to be)
       "Contains all impressions for all publishers, grouped by referrers " >> {
         outputBuffer.size must_== 2
         outputBuffer must contain(("www.example.com", 2))
